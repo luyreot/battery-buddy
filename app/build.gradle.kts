3cb8a19c70a4a962f434fb01b70c64c707929627
luyreot <luyreot@luyreot.com>
2024-12-30 22:18:30 +0200

    Add material icons extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons)
     implementation(libs.google.accompanist)
 
     debugImplementation(libs.androidx.ui.tooling)
